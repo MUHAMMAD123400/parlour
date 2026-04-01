ab8d474798266dde373961cbe1a4327804a24558
Muhammad <muhammadsaad.alphatach@gmail.com>
2026-04-01 16:44:49 +0500

    add new roles seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call([
             PermissionSeeder::class,
+            RoleSeeder::class,
             UsersSeeder::class,
         ]);
         // User::factory()->create([
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class RoleSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Role::updateOrCreate(
+            [
+                'name' => 'super_admin',
+                'guard_name' => 'api',
+            ],
+            [
+                'description' => 'System super admin role',
+            ]
+        );
+
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+    }
+}
